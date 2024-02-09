5966c72f750203654a2582008aba5e136b389163
derekbeistad <derekbeistad@gmail.com>
2024-02-08 19:28:38 -0600

    Finished Q7

--- scripts/data_analyst_jobs_derekbeistad.sql
+++ scripts/data_analyst_jobs_derekbeistad.sql
@@ -25,3 +25,31 @@ FROM data_analyst_jobs
 WHERE location IN ('TN', 'KY');
 -- 		A. 81
 
+-- 4. How many postings in Tennessee have a star rating above 4?
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE star_rating > 4;
+-- 		A. 1248
+
+-- 5. How many postings in the dataset have a review count between 500 and 1000?
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE review_count BETWEEN 500 AND 1000;
+-- 		A. 453
+
+-- 6. Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
+SELECT location AS state, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+GROUP BY location
+ORDER BY AVG(star_rating) DESC;
+-- 		A. NE
+
+-- 7. Select unique job titles from the data_analyst_jobs table. How many are there?
+SELECT DISTINCT title
+FROM data_analyst_jobs;
+
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
+-- 		A. 881
+
+-- 8. 
