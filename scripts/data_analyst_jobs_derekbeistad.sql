123e4d53c94dc408c7d11cd27eb3d55b03620f1d
derekbeistad <derekbeistad@gmail.com>
2024-02-10 09:53:05 -0600

    added new table without duplicated data import

--- scripts/data_analyst_jobs_derekbeistad.sql
+++ scripts/data_analyst_jobs_derekbeistad.sql
@@ -4,83 +4,84 @@ LIMIT 5;
 
 -- 1. How many rows are in the data_analyst_jobs table?
 SELECT	COUNT(*)
-FROM data_analyst_jobs;
--- 		A. 5379
+FROM indeed_analyst_jobs;
+-- 		A. 1793
 
 -- 2. Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
 SELECT *
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 LIMIT 10;
 -- 		A. ExxonMobil
 
 -- 3A. How many postings are in Tennessee?
 SELECT COUNT(*)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE location = 'TN';
--- 		A. 63
+-- 		A. 21
 
 -- 3B. How many are there in either Tennessee or Kentucky?
 SELECT COUNT(*)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE location IN ('TN', 'KY');
--- 		A. 81
+-- 		A. 27
 
 -- 4. How many postings in Tennessee have a star rating above 4?
 SELECT COUNT(*)
-FROM data_analyst_jobs
-WHERE star_rating > 4;
--- 		A. 1248
+FROM indeed_analyst_jobs
+WHERE location ILIKE 'TN'
+AND star_rating > 4;
+-- 		A. 3
 
 -- 5. How many postings in the dataset have a review count between 500 and 1000?
 SELECT COUNT(*)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE review_count BETWEEN 500 AND 1000;
--- 		A. 453
+-- 		A. 151
 
 -- 6. Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 SELECT location AS state, AVG(star_rating) AS avg_rating
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 GROUP BY location
 ORDER BY AVG(star_rating) DESC;
 -- 		A. NE
 
 -- 7A. Select unique job titles from the data_analyst_jobs table.
 SELECT DISTINCT title
-FROM data_analyst_jobs;
+FROM indeed_analyst_jobs;
 
 -- 7B. How many are there?
 SELECT COUNT(DISTINCT title)
-FROM data_analyst_jobs;
+FROM indeed_analyst_jobs;
 -- 		A. 881
 
 -- 8. How many unique job titles are there for California companies?
 SELECT COUNT(DISTINCT title)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE location = 'CA';
 -- 		A. 230
 
 -- 9A. Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations.
 SELECT company, AVG(star_rating) AS avg_rating
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE review_count > 5000
 GROUP BY company;
 
 -- 9B. How many companies are there with more that 5000 reviews across all locations?
 SELECT COUNT(DISTINCT company)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE review_count > 5000;
 -- 		A. 40
 
 -- 10A. Add the code to order the query in #9 from highest to lowest average star rating. 
 SELECT company, AVG(star_rating) AS avg_rating
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE review_count > 5000
 GROUP BY company
 ORDER BY avg_rating DESC;
 
 -- 10B. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 SELECT company, AVG(star_rating) AS avg_rating
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE review_count > 5000
 GROUP BY company
 ORDER BY avg_rating DESC
@@ -89,18 +90,18 @@ LIMIT 6;
 
 -- 11A. Find all the job titles that contain the word ‘Analyst’. 
 SELECT title
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE title ILIKE '%analyst%';
 
 -- 11B. How many different job titles are there?
 SELECT COUNT(DISTINCT title)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE title ILIKE '%analyst%';
 -- 		A. 774
 
 -- 12A. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’?
 SELECT COUNT(DISTINCT title)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE title ILIKE '%analyst%'
 OR title ILIKE '%analytics%';
 -- 		A. 877
@@ -108,7 +109,7 @@ OR title ILIKE '%analytics%';
 -- 12B. What word do these positions have in common?
 -- 		A. 'analy'
 SELECT COUNT(DISTINCT title)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE title ILIKE '%analy%';
 -- 		A. 877
 
@@ -116,7 +117,7 @@ WHERE title ILIKE '%analy%';
 --  - Disregard any postings where the domain is NULL. 
 --  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
 SELECT domain, COUNT(title)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE skill ILIKE '%sql%'
 AND days_since_posting > (7*3)
 AND domain IS NOT NULL
@@ -125,7 +126,7 @@ ORDER BY COUNT(title) DESC;
 
 --  - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
 SELECT domain, COUNT(title)
-FROM data_analyst_jobs
+FROM indeed_analyst_jobs
 WHERE skill ILIKE '%sql%'
 AND days_since_posting > (7*3)
 AND domain IS NOT NULL
